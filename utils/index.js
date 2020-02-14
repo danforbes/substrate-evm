ddb17b986f727730ebc2c1ddc959d186a569fc1f
Dan Forbes <dan@danforbes.dev>
2020-02-14 07:36:52 -0800

    Restructure Utils

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,27 @@
-const web3 = require('web3');
+const help =
+`--erc20-slot <slot> <address>: Calculate the storage slot for an (EVM) address's ERC-20 balance, where <slot> is the storage slot for the ERC-20 balances map.
+--evm-address <address>: Calculate the EVM address that corresponds to a native Substrate address.
+--help: Print this message.`;
+
+if (process.argv.length < 3) {
+  console.error('Please provide a command.');
+  console.error(help);
+  process.exit(9);
+}
 
 const command = process.argv[2];
 switch (command) {
-  case "--erc20-balance":
-    const slot = process.argv[3];
-    const address = process.argv[4];
-    const mapStorageSlot = slot.padStart(64, '0');
-    const mapKey = address.substring(2).padStart(64, '0');
-    const balanceStorageSlot = web3.utils.sha3('0x'.concat(mapKey.concat(mapStorageSlot)));
-    console.log(`Storage slot for balance of ${address} with balance map at slot ${slot}: ${balanceStorageSlot}`);
+  case "--erc20-slot":
+    console.log(require('./erc20-slot')());
+    break;
+  case "--evm-address":
+    console.log(require('./evm-address')());
+    break;
+  case "--help":
+    console.log(help);
     break;
   default:
-    console.log(`Unknown command: ${command}.`);
+    console.error(`Unrecognized command: ${command}.`);
+    console.error(help);
+    process.exit(9);
 }
